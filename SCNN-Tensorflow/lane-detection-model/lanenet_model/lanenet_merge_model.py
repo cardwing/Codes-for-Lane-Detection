f464fc8c0135f87362e8b740056ad36260fdcc2e
HOU Yuenan <Yuenan_Hou@163.com>
2018-11-28 11:59:57 +0800

    Update lanenet_merge_model.py

--- SCNN-Tensorflow/lane-detection-model/lanenet_model/lanenet_merge_model.py
+++ SCNN-Tensorflow/lane-detection-model/lanenet_model/lanenet_merge_model.py
@@ -78,28 +78,20 @@ class LaneNet(cnn_basenet.CNNBaseModel):
                 shape=[binary_label.get_shape().as_list()[0],
                        binary_label.get_shape().as_list()[1] * binary_label.get_shape().as_list()[2]])
             binary_label_reshape = tf.one_hot(binary_label_reshape, depth=5)
-
             class_weights = tf.constant([[0.4, 1.0, 1.0, 1.0, 1.0]])
             weights_loss = tf.reduce_sum(tf.multiply(binary_label_reshape, class_weights), 2)
-       
             binary_segmenatation_loss = tf.losses.softmax_cross_entropy(onehot_labels=binary_label_reshape, logits=decode_logits_reshape, weights=weights_loss)
-
             binary_segmenatation_loss = tf.reduce_mean(binary_segmenatation_loss)
 
             # Compute the sigmoid loss
 
             existence_logit = inference_ret['existence_output']
-
-            # print(existence_label)
-            
             existence_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=existence_label, logits=existence_logit)
-
             existence_loss = tf.reduce_mean(existence_loss)
  
             # Compute the overall loss
 
             total_loss = binary_segmenatation_loss + 0.1 * existence_loss
-
             ret = {
                 'total_loss': total_loss,
                 'instance_seg_logits': decode_logits,
